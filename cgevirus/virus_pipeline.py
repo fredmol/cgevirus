e26cac8be1e498a242835503592fdfbd7be81669
MBHallgren <s153002@student.dtu.dk>
2023-11-20 13:21:38 +0100

    test2

--- cgevirus/virus_pipeline.py
+++ cgevirus/virus_pipeline.py
@@ -6,20 +6,20 @@ from cgevirus import kma
 
 
 def virus_pipeline(args):
-    os.system('mkdir ' + args.output)
+    #os.system('mkdir ' + args.output)
     # Check if kma is installed
-    kma.KMARunner(args.input,
-              args.output + "/virus_alignment",
-              args.db_dir + '/virus_db/virus_db',
-              "-ont -ca -1t1 -mem_mode").run()
+    #kma.KMARunner(args.input,
+    #          args.output + "/virus_alignment",
+    #          args.db_dir + '/virus_db/virus_db',
+    #          "-ont -ca -1t1 -mem_mode").run()
 
-    kma.KMARunner(args.input,
-              args.output + "/cdd",
-              args.db_dir + '/cdd_db/cdd_db',
-              "-ont -ca -1t1 -mem_mode").run()
+    #kma.KMARunner(args.input,
+    #          args.output + "/cdd",
+    #          args.db_dir + '/cdd_db/cdd_db',
+    #          "-ont -ca -1t1 -mem_mode").run()
 
-    cmd = 'prokka -outdir {}/ --centre virus_alignment --prefix prokka_results {}/virus_alignment.fsa'.format(args.output, args.output)
-    os.system(cmd)
+    #cmd = 'prokka -outdir {}/ --centre virus_alignment --kingdom Viruses --prefix prokka_results {}/virus_alignment.fsa --force'.format(args.output, args.output)
+    #os.system(cmd)
 
 
     #conda Prokka
