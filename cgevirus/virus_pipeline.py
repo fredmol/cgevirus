5543c89f98d5c97880eae2e931db76425961f739
MBHallgren <s153002@student.dtu.dk>
2023-11-22 15:21:28 +0100

    v1

--- cgevirus/virus_pipeline.py
+++ cgevirus/virus_pipeline.py
@@ -6,12 +6,20 @@ from cgevirus import kma
 
 
 def virus_pipeline(args):
+    if args.db_dir is None:
+        if not os.path.exists('/opt/cge/cge_db'):
+            sys.exit('Please install the cge_db. It should be located in /opt/cge/cge_db')
+        else:
+            args.db_dir = '/opt/cge/cge_db'
+
     os.system('mkdir ' + args.output)
     # Check if kma is installed
     kma.KMARunner(args.input,
               args.output + "/virus_alignment",
               args.db_dir + '/virus_db/virus_db',
               "-ont -ca -1t1 -mem_mode").run()
+
+    highest_scoring_hit = get_highest_scoring_hit_template(args.output + "/virus_alignment.res")
 
     kma.KMARunner(args.input,
               args.output + "/cdd",
@@ -21,13 +29,36 @@ def virus_pipeline(args):
     cmd = 'prokka -outdir {}/ --centre virus_alignment --kingdom Viruses --prefix prokka_results {}/virus_alignment.fsa --force'.format(args.output, args.output)
     os.system(cmd)
 
-
-    #pathogenicity?
-
-    #Compile results TBD
-
-    #Same as for cgeisolate, compile all results into a file.
-        #Should be the CDD results, the prokka results, and the kma results (virus with top alignment score).
-
-
     return 'virus_pipeline'
+
+
+def read_tab_separated_file(file_path):
+    with open(file_path, 'r') as file:
+        reader = csv.DictReader(file, delimiter='\t')
+        return list(reader)
+
+def format_results_section(results, section_title):
+    report = f"{section_title}:\n"
+    report += "-" * 60 + "\n"
+    for result in results:
+        report += f"Template: {result['#Template']}\n"
+        report += f"Identity: {result['Template_Identity'].strip()}, Coverage: {result['Template_Coverage'].strip()}, Depth: {result['Depth'].strip()}\n\n"
+    return report
+
+def get_highest_scoring_hit_template(file_path):
+    with open(file_path, 'r') as file:
+        reader = csv.DictReader(file, delimiter='\t')
+        highest_scoring_hit = None
+        max_score = float('-inf')  # Initialize with the smallest possible number
+
+        for row in reader:
+            try:
+                score = float(row['Score'])
+                if score > max_score:
+                    highest_scoring_hit = row
+                    max_score = score
+            except ValueError:
+                # Handle the case where the score is not a number
+                continue
+
+    return highest_scoring_hit['#Template'] if highest_scoring_hit else None
