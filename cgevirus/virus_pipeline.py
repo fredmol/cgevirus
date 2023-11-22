6f9a8b1e5a41ed5a8cc57cabf129f4c96bfa0a6e
MBHallgren <s153002@student.dtu.dk>
2023-11-22 15:27:33 +0100

    v3

--- cgevirus/virus_pipeline.py
+++ cgevirus/virus_pipeline.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import subprocess
+import csv
 
 from cgevirus import kma
 
@@ -14,17 +15,17 @@ def virus_pipeline(args):
 
     os.system('mkdir ' + args.output)
     # Check if kma is installed
-    kma.KMARunner(args.input,
-              args.output + "/virus_alignment",
-              args.db_dir + '/virus_db/virus_db',
-              "-ont -ca -1t1 -mem_mode -t 8").run()
+    #kma.KMARunner(args.input,
+    #          args.output + "/virus_alignment",
+    #          args.db_dir + '/virus_db/virus_db',
+    #          "-ont -ca -1t1 -mem_mode -t 8").run()
 
     highest_scoring_hit = get_highest_scoring_hit_template(args.output + "/virus_alignment.res")
 
-    kma.KMARunner(args.input,
-              args.output + "/cdd",
-              args.db_dir + '/cdd_db/cdd_db',
-              "-ont -ca -1t1 -mem_mode -t 8").run()
+    #kma.KMARunner(args.input,
+    #          args.output + "/cdd",
+    #          args.db_dir + '/cdd_db/cdd_db',
+    #          "-ont -ca -1t1 -mem_mode -t 8").run()
 
     cmd = 'prokka -outdir {}/ --centre virus_alignment --kingdom Viruses --prefix prokka_results {}/virus_alignment.fsa --force'.format(args.output, args.output)
     os.system(cmd)
