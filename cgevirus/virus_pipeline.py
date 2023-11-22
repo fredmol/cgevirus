4575fc912e2dd06ca7d851795bb45ddb511314ab
MBHallgren <s153002@student.dtu.dk>
2023-11-22 15:46:33 +0100

    v7

--- cgevirus/virus_pipeline.py
+++ cgevirus/virus_pipeline.py
@@ -14,19 +14,19 @@ def virus_pipeline(args):
             args.db_dir = '/opt/cge/cge_db'
 
     os.system('mkdir ' + args.output)
-    kma.KMARunner(args.input,
-              args.output + "/virus_alignment",
-              args.db_dir + '/virus_db/virus_db',
-              "-ont -ca -1t1 -mem_mode -t 8").run()
+    #kma.KMARunner(args.input,
+    #          args.output + "/virus_alignment",
+    #          args.db_dir + '/virus_db/virus_db',
+    #          "-ont -ca -1t1 -mem_mode -t 8").run()
     highest_scoring_hit = get_highest_scoring_hit_template(args.output + "/virus_alignment.res")
 
-    kma.KMARunner(args.input,
-              args.output + "/cdd",
-              args.db_dir + '/cdd_db/cdd_db',
-              "-ont -ca -1t1 -mem_mode -t 8").run()
+    #kma.KMARunner(args.input,
+    #          args.output + "/cdd",
+    #          args.db_dir + '/cdd_db/cdd_db',
+    #          "-ont -ca -1t1 -mem_mode -t 8").run()
 
-    cmd = 'prokka -outdir {}/ --centre virus_alignment --kingdom Viruses --prefix prokka_results {}/virus_alignment.fsa --force'.format(args.output, args.output)
-    os.system(cmd)
+    #cmd = 'prokka -outdir {}/ --centre virus_alignment --kingdom Viruses --prefix prokka_results {}/virus_alignment.fsa --force'.format(args.output, args.output)
+    #os.system(cmd)
 
     report = create_virus_report(args, highest_scoring_hit)
     with open(args.output + "/virus_pipeline_report.txt", "w") as f:
@@ -61,7 +61,7 @@ def create_virus_report(args, highest_scoring_hit):
     return report
 
 def format_prokka_results(prokka_file):
-    prokka_results = read_tab_separated_file(prokka_file, delimiter='\t')
+    prokka_results = read_tab_separated_file(prokka_file)  # Removed the delimiter argument
     report = "Prokka Results:\n"
     report += "-" * 60 + "\n"
     for result in prokka_results:
